03fa41bc72ac962a17e2d4ca1d1c960a6d07150e
MalindaIlesinghe <malindailesinghe@gmail.com>
2026-05-15 12:06:00 +0530

    created login and profile html files

--- src/main/java/com/grocery/GroceryManagementApplication.java
+++ src/main/java/com/grocery/GroceryManagementApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GroceryManagementApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GroceryManagementApplication.class, args);
 	}
-
+	
 }
